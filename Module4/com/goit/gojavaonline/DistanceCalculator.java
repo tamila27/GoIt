b6f176cfae84dbfa1ead586ecc196d22f4e89b86
Tamila Asanova <tamila@Timurs-iMac.local>
2016-03-11 16:59:15 +0200

    Runner_class_added

--- Module4/com/goit/gojavaonline/DistanceCalculator.java
+++ Module4/com/goit/gojavaonline/DistanceCalculator.java
@@ -1,7 +1,7 @@
 package com.goit.gojavaonline;
 
 public class DistanceCalculator {
-    public double calculateDistanceBetween2Points(double x1, double x2, double y1, double y2){
+    public static double calculateDistanceBetween2Points(double x1, double x2, double y1, double y2){
         double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         return distance;
     }
--- Module4/com/goit/gojavaonline/TemperatureCalculator.java
+++ Module4/com/goit/gojavaonline/TemperatureCalculator.java
@@ -1,12 +1,12 @@
 package com.goit.gojavaonline;
 
 public class TemperatureCalculator {
-    public double fahrenheitToCelsiusConverter(double fahrenheitDegrees){
+    public static double fahrenheitToCelsiusConverter(double fahrenheitDegrees){
         double celsiusDegrees = (5/9) * (fahrenheitDegrees - 32);
         return celsiusDegrees;
     }
 
-    public double celsiusToFahrenheitConverter(double celsiusDegrees){
+    public static double celsiusToFahrenheitConverter(double celsiusDegrees){
         double fahrenheitDegrees = (9/5) * celsiusDegrees + 32;
         return fahrenheitDegrees;
     }
