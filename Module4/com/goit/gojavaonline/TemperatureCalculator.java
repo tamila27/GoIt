49e2eb125f1d788a1c6822646c0645d99b0d079d
tamila <tamilaasanova@gmail.com>
2016-03-21 10:26:55 +0200

    division_bug_fixed

--- Module4/com/goit/gojavaonline/TemperatureCalculator.java
+++ Module4/com/goit/gojavaonline/TemperatureCalculator.java
@@ -2,12 +2,12 @@ package com.goit.gojavaonline;
 
 public class TemperatureCalculator {
     public static double fahrenheitToCelsiusConverter(double fahrenheitDegrees){
-        double celsiusDegrees = (5/9) * (fahrenheitDegrees - 32);
+        double celsiusDegrees = (5.0/9) * (fahrenheitDegrees - 32);
         return celsiusDegrees;
     }
 
     public static double celsiusToFahrenheitConverter(double celsiusDegrees){
-        double fahrenheitDegrees = (9/5) * celsiusDegrees + 32;
+        double fahrenheitDegrees = (9.0/5) * celsiusDegrees + 32;
         return fahrenheitDegrees;
     }
 }
